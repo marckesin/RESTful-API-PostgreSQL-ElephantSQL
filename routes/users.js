c511e11fd1f31cceeff1fc6c7adf4b3252735562
marckesin <192.168.2.1@protonmail.com>
2022-02-15 10:07:44 -0300

    refatorado users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const db = require("../db/database.config");
 const usersController = require("../controllers/usersController");
 
 const router = express.Router();
